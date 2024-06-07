477fda43cd77b839a7df63ba4864daa198c7d875
surkanghyun <qorrnfn2000@naver.com>
2024-06-07 13:21:31 +0900

    #4 임시커밋

--- App.js
+++ App.js
@@ -1,12 +1,34 @@
 import { StyleSheet, View } from 'react-native';
-
 import AuthContent from './components/Auth/AuthContent';
+import { createStackNavigator } from '@react-navigation/stack';
+import LoginScreen from './screens/LoginScreen';
+import SignupScreen from './screens/SignupScreen';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
+import { NavigationContainer } from '@react-navigation/native';
+
+const Stack = createStackNavigator();
+
+const AuthStack = () => {
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        //Header 블록에 대한 스타일
+        headerStyle: { backgroundColor: Colors.primary500 },
+        //Header 의 텍스트 , 버튼 색상
+        headerTintColor: 'white',
+      }}
+    >
+      <Stack.Screen name='Login' component={LoginScreen} />
+      <Stack.Screen name='SignUp' component={SignupScreen} />
+    </Stack.Navigator>
+  );
+};
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <AuthContent />
-    </View>
+    <NavigationContainer>
+      <AuthStack />
+    </NavigationContainer>
   );
 }
 
